6f425dd9e1e014ae1bbc560c5a5deec44c343afd
Edouard <edouard.legoupil@gmail.com>
2017-04-27 10:04:43 +0300

    Resplit code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Transaction Analysis
+
+Analysis of cash distribution transaction.
+
+Ideas to look at:
+
+ * [Mapping connection with great arc circle](http://flowingdata.com/2011/05/11/how-to-map-connections-with-great-circles/)
+
+ * [Drawing catchment areas using probabilistic approach: the Huff model](https://cran.r-project.org/web/packages/SpatialPosition/vignettes/SpatialPosition.html)
+
+ * [Optimal site selection](https://dirkschumacher.github.io/ompr/articles/problem-warehouse-location.html)
+
+ * Work from WFP / Leiden Univesity [here](http://documents.wfp.org/stellent/groups/public/documents/ena/wfp291114.pdf)  or [there](https://doc-0k-1k-docs.googleusercontent.com/docs/securesc/5ru92vmpp2v64k2hkrs2v256896id376/175fki2rnn8764i8tut9ciq32agrbqsf/1489255200000/10687521860128767674/07495711428163271540/0B4FNu0aaAFzNQl9zVG5WZVEwUFk?e=download)
+
+
+
+
--- code/0-packages.R
+++ code/0-packages.R
@@ -1,3 +1,38 @@
+## install packages if not yet installed
+
+packages <- c( # "leaflet", # We need latest leaflet package from Github, as CRAN package is too old.
+                "devtools","viridis",
+                "RColorBrewer",
+                "plyr",
+                "htmlwidgets",
+                "htmltools",
+               # "leaflet.extras", ## need to be installed from Github
+                "rgdal",
+                "sf",
+                "sp",
+                "spatialEco",
+                "rgeos",
+                "ggplot2",
+                "data.table",
+                "dplyr",
+                "plotly",
+                "webshot",
+                "colorspace",
+                "geosphere")
+
+## identify packages not installed yet
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())))
+}
+
+rm(packages)
+
+library(devtools)
+
+# We need latest leaflet package from Github, as CRAN package is too old.
+#devtools::install_github('rstudio/leaflet')
+#devtools::install_github('bhaskarvk/leaflet.extras')
+
 
 ### Load packages
 
